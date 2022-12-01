c6877ca4e4a3aa0a675ca7597d29fceeb19fc42e
tapendra jaishi <tapendra.jaishi2058@gmail.com>
2022-12-01 11:06:16 +0545

    image

--- lib/vendor/vendorCreatePost.dart
+++ lib/vendor/vendorCreatePost.dart
@@ -1,5 +1,6 @@
-import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
@@ -25,142 +26,147 @@ class _VendorCreatePostState extends State<VendorCreatePost> {
 
 
 
+
+  String base64image = "";
+  File? _image;
+  Future _getfromgallery() async{
+
+    PickedFile? image;
+    final pickedfile  = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if(pickedfile!= null){
+      this.setState(() {
+        _image = File(pickedfile!.path);
+      });
+    }else {
+      if (kDebugMode) {
+        print('No image selected.');
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          body: ClipRect(
-            child: Center(
-              child: Container(
-                height: 1000,
-                width: MediaQuery.of(context).size.width*0.9,
-
-                child: Column(
-                  children: [
-                    Text("Add image"),
-                    Expanded(
-                      flex: 2,
-                      child: Container(
-                        width: 300,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          border: Border.all(color: Colors.deepPurple),
+          body: Container(
+            child: Form(
+                  key: _formkey,
+                  child: ListView(
+                      padding: EdgeInsets.all(20),
+                      children: [
+                        SizedBox(height: 35,),
+                        DropdownButtonFormField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              )
+                          ),
+                          hint: Text("Select Category"),
+                          value: selectval,
+                          onChanged: (value) {
+                            setState(() {
+                              selectval = value.toString();
+                            });
+                          },
+                          items: listitems.map((itemone) {
+                            return DropdownMenuItem(value: itemone, child: Text(itemone));
+                          }).toList(),
                         ),
-                        child: Center(
+                        SizedBox(height: 15,),
+                        TextFormField(
+                          keyboardType: TextInputType.text,
+                          controller: addproduct,
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return 'Enter prodect name';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)
+                            ),
+                            hintText: 'Product name',
+                            labelText: 'Product',
+                          ),
+                        ),
+                        SizedBox(height: 15,),
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: addprice,
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return 'Enter price';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)
+                            ),
+                            hintText: 'Product Price',
+                            labelText: 'Price',
+                          ),
+                        ),
+                        SizedBox(height: 15,),
+                        TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: addquantity,
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return 'Enter quentity';
+                            }
+                          },
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)
+                            ),
+                            hintText: 'Quantity',
+                            labelText: 'Quantity',
+                          ),
+                        ),
+                        SizedBox(height: 15,),
+
+                        Container(
+                          height: 300,
                           child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              Expanded(
-                                  child: Center(child: Text("No image selected"))),
-                              ElevatedButton(
-                                  onPressed: () {}, child: Text("Select image")),
+                              ElevatedButton(onPressed: (){
+                                _getfromgallery();
+                              }, child:
+                              Text("Pick image",style: TextStyle(color: Colors.orange),),
+                              ),
+                              Expanded(child: buildGridview())
                             ],
                           ),
                         ),
-                      ),
-                    ),
 
-                    Expanded(
-                      flex: 6,
-                      child: Form(
-                        key: _formkey,
-                        child: ListView(
-                            padding: EdgeInsets.all(20),
-                            children: [
-                              SizedBox(height: 15,),
-                              DropdownButtonFormField(
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(15),
-                                    )
-                                ),
-                                hint: Text("Select Category"),
-                                value: selectval,
-                                onChanged: (value) {
-                                  setState(() {
-                                    selectval = value.toString();
-                                  });
-                                },
-                                items: listitems.map((itemone) {
-                                  return DropdownMenuItem(value: itemone, child: Text(itemone));
-                                }).toList(),
-                              ),
-                              SizedBox(height: 15,),
-                              TextFormField(
-                                keyboardType: TextInputType.text,
-                                controller: addproduct,
-                                validator: (value){
-                                  if(value!.isEmpty){
-                                    return 'Enter prodect name';
-                                  }
-                                },
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(15)
-                                  ),
-                                  hintText: 'Product name',
-                                  labelText: 'Product',
-                                ),
-                              ),
-                              SizedBox(height: 15,),
-                              TextFormField(
-                                keyboardType: TextInputType.number,
-                                controller: addprice,
-                                validator: (value){
-                                  if(value!.isEmpty){
-                                    return 'Enter price';
-                                  }
-                                },
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(15)
-                                  ),
-                                  hintText: 'Product Price',
-                                  labelText: 'Price',
-                                ),
-                              ),
-                              SizedBox(height: 15,),
-                              TextFormField(
-                                keyboardType: TextInputType.number,
-                                controller: addquantity,
-                                validator: (value){
-                                  if(value!.isEmpty){
-                                    return 'Enter quentity';
-                                  }
-                                },
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(15)
-                                  ),
-                                  hintText: 'Quantity',
-                                  labelText: 'Quantity',
-                                ),
-                              ),
-                              SizedBox(height: 15,),
-
-
-                              MaterialButton(
-                                  color: Colors.orange,
-                                  onPressed: (){
-                                    post();
-                                  },
-                                  child: Text("Post",style: TextStyle(
-                                      fontSize: 19
-                                  ),)
-                              ),
-                            ]
+                        MaterialButton(
+                            color: Colors.orange,
+                            onPressed: (){
+                              post();
+                            },
+                            child: Text("Post",style: TextStyle(
+                                fontSize: 19
+                            ),)
                         ),
-                      ),
-                    ),
-                  ],
-                )
+                      ]
+                  ),
+                ),
 
-              ),
-            ),
           ),
         )
     );
   }
+
+  Widget buildGridview(){
+    return Container(
+      width: 400,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.black26,
+      ),
+      child: _image != null ? Image.file(_image!.absolute) : Icon(Icons.image),
+    );
+  }
+
 
   void post(){
     if(_formkey.currentState!.validate()){
